e25aae7f7b5ec7c7755c6281f12840023bcbd226
nickyobreykov <nicky.obreykov@gmail.com>
2014-12-01 00:10:57 +0100

    achievement activity

--- app/src/main/java/com/liquidafro/tapten/ShopAndAchievementActivity.java
+++ app/src/main/java/com/liquidafro/tapten/ShopAndAchievementActivity.java
@@ -4,14 +4,26 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class ShopAndAchievementActivity extends Activity {
 
+    TextView testtv;
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_shop_and_achievement);
+
+
+
     }
 
 
---
+++ app/src/main/java/com/liquidafro/tapten/ShopAndAchievementCanvas.java
@@ -0,0 +1,27 @@
+package com.liquidafro.tapten;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+/**
+ * Created by bunibun on 01/12/14.
+ */
+public class ShopAndAchievementCanvas extends View{
+
+    Paint paint;
+
+    public ShopAndAchievementCanvas(Context context){
+        super(context);
+
+        paint = new Paint();
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+}
